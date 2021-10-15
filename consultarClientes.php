78d088db3fbbeaebd86cb8eb2141b46f8b1a5542
YasmiinMello <89152771+YasmiinMello@users.noreply.github.com>
2021-10-15 17:57:16 -0300

    Add files via upload

---
+++ consultarClientes.php
@@ -0,0 +1,87 @@
+<?php
+require_once("controller/ControllerCadastro.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema de Agendamento - Clientes</title>
+    <link rel="stylesheet" href="bootstrap-5.0.1-dist/bootstrap-5.0.1-dist/css/bootstrap.css">
+	<link rel="stylesheet" href="css/estilo.css">
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="bootstrap-5.0.1-dist/bootstrap-5.0.1-dist/js/bootstrap.js"></script>
+    <script>
+		function confirmDelete(delUrl) {
+  			if (confirm("Deseja apagar o registro?")) {
+   				document.location = delUrl;
+   			}  
+		}
+	</script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light col-12" style="background-color: #e3f2fd;">
+                <a class="navbar-brand" href="#">SISTEMA WEB</a>
+                <button  class="navbar-toggler" type="button" data-toggle="collapse" data-target="
+                #navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="toggle
+                navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.php">Cadastrar<span class="sr-only"></span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="consultarClientes.php">Consultar</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="row">
+            <div class="card mb-3 col-12">
+                <div class="card-body">
+                    <h5 class="card-title">Consultar - Agendamento de Potenciais Clientes</h5>
+                    <table class="table table-responsive table-hover col-12" style="width: auto;">
+                        <thead class="table-active bg-light">
+                            <tr>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Telefone</th>
+                                <th scope="col">Origem</th>
+                                <th scope="col">Contato</th>
+                                <th scope="col">Observação</th>
+                                <th scope="col">Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody id="TableData">
+						<?php
+							$controller = new ControllerCadastro();
+							$resultado = $controller->listar(0);
+							for($i=0;$i<count($resultado);$i++){ 
+						?>
+								<tr>
+									<td scope="col"><?php echo $resultado[$i]['nome']; ?></td>
+									<td scope="col"><?php echo $resultado[$i]['telefone']; ?></td>
+									<td scope="col"><?php echo $resultado[$i]['origem']; ?></td>
+									<td scope="col"><?php echo $resultado[$i]['data_contato']; ?></td>
+									<td scope="col"><?php echo $resultado[$i]['observacao']; ?></td>
+									<td scope="col">
+										<button type="button" class="btn btn-outline-primary" style="width: 72px;">Editar</button>
+										<button type="button" class="btn btn-outline-primary" style="width: 72px;">Excluir</button>
+									</td>
+								</tr>
+						<?php
+							}
+						?>
+						</tbody>
+                    </table>
+                    <button type="button" class="btn btn-outline-dark">Buscar Agendamento</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
